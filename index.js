fda342c2a43e52f9b6f8671244e2908031eced0b
RAVI KUMAR <ravikumar444411@gmail.com>
2021-02-22 01:57:11 +0530

    make it change3

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
+const adminRoute = require('./api/routes/admin')
 
 app.set('port',process.env.PORT || 8000);
 var port = app.get('port');
+
+app.use('/admin',adminRoute);
 
 app.get('/',(req,res)=>{
     res.send('<h1>Hello world 3 </h1>');
@@ -11,6 +14,19 @@ app.get('/check',(req,res)=>{
     res.send('<h1>check page </h1>');
     res.json({msg:'it fine'});
 })
+
+
+
+
+mongoose.connect('mongodb+srv://ravi:Ravi12345@cluster0.hioak.mongodb.net/test?retryWrites=true&w=majority')
+
+
+mongoose.connection.on('error',err=>{
+    console.log('error found')
+});
+mongoose.connection.on('connected',connected=>{
+    console.log('conntected database')
+});
 
 
 
