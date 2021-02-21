d441f783d7c4162653186ace961723a98586b39f
RAVI KUMAR <ravikumar444411@gmail.com>
2021-02-22 01:11:58 +0530

    make it change

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port',process.env.PORT || 8000);
 var port = app.get('port');
 
 app.get('/',(req,res)=>{
-    res.send('<h1>Hello world </h1>');
+    res.send('<h1>Hello world 2 </h1>');
 })
 
 
